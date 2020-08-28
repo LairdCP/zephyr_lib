66863a08ae0f8ad2fe22bbe3d5169dde4f358963
Adam Ruehl <adam.ruehl@lairdconnect.com>
2020-08-28 14:39:44 -0400

    drivers: lis2dh: add customer lis2dh driver for the mg100
    this change adds a custom driver for the lis2dh/lis3dh. this is
    due to the fact that mg100 has the part connected to the interrupt
    pins in a non-standard way, utilizing only the INT1 pin. in adddition
    the mg100 requires access to the temperature sensor.
    
    Bug 17087

---
+++ drivers/mg100_lis2dh/mg100_lis2dh.c
@@ -0,0 +1,440 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ * Copyright (c) 2020 Laird Connectivity
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT st_lis2dh
+
+
+#include <init.h>
+#include <sys/byteorder.h>
+#include <sys/__assert.h>
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(lis2dh, CONFIG_SENSOR_LOG_LEVEL);
+#include "mg100_lis2dh.h"
+
+#define INVALID_TEMPERATURE	-127
+#define TEMP_REFERENCE	25
+
+#define ACCEL_SCALE(sensitivity)			\
+	((SENSOR_G * (sensitivity) >> 14) / 100)
+
+/*
+ * Use values for low-power mode in DS "Mechanical (Sensor) characteristics",
+ * multiplied by 100.
+ */
+static const u32_t lis2dh_reg_val_to_scale[] = {
+#if DT_NODE_HAS_STATUS(DT_INST(0, st_lsm303agr_accel), okay)
+	ACCEL_SCALE(1563),
+	ACCEL_SCALE(3126),
+	ACCEL_SCALE(6252),
+	ACCEL_SCALE(18758),
+#else
+	ACCEL_SCALE(1600),
+	ACCEL_SCALE(3200),
+	ACCEL_SCALE(6400),
+	ACCEL_SCALE(19200),
+#endif
+};
+
+static int lis2dh_channel_get_temp(struct device *dev, struct sensor_value *val)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	int ret = -EBADMSG;
+
+	if (lis2dh->temp_sample != INVALID_TEMPERATURE) {
+		val->val1 = lis2dh->temp_sample;
+		val->val2 = 0;
+		ret = 0;
+	}
+
+	return ret;
+}
+
+static int lis2dh_sample_fetch_temp(struct device *dev)
+{
+	int ret = 0;
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	u8_t temp_raw[sizeof(u16_t)];
+
+	/*
+	 * the LIS2DH/LIS3DH requires a 2 byte read for the temperature value
+	 *  even though only the _H register has valid data. the _L and _H
+	 *  registers are consecutive, so a burst read will work here.
+	 */
+	ret = lis2dh->hw_tf->read_data(dev, LIS2DH_REG_ADC3_L,
+					  temp_raw,
+					  sizeof(temp_raw));
+
+	if (ret < 0) {
+		LOG_WRN("Failed to fetch raw temp sample");
+		ret = -EIO;
+		lis2dh->temp_sample = INVALID_TEMPERATURE;
+	}
+	else {
+		/*
+		 * LIS2DH_REG_ADC3_H contains a delta value for the
+		 *  temperature that must be added to the reference temperature set
+		 *  for your board to return an absolute temperature in celsius.
+		 */
+		lis2dh->temp_sample = TEMP_REFERENCE + temp_raw[1];
+	}
+
+	return ret;
+}
+
+static void lis2dh_convert(s16_t raw_val, u32_t scale,
+			   struct sensor_value *val)
+{
+	s32_t converted_val;
+
+	/*
+	 * maximum converted value we can get is: max(raw_val) * max(scale)
+	 *	max(raw_val >> 4) = +/- 2^11
+	 *	max(scale) = 114921
+	 *	max(converted_val) = 235358208 which is less than 2^31
+	 */
+	converted_val = (raw_val >> 4) * scale;
+	val->val1 = converted_val / 1000000;
+	val->val2 = converted_val % 1000000;
+}
+
+static int lis2dh_channel_get(struct device *dev,
+			      enum sensor_channel chan,
+			      struct sensor_value *val)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	int ofs_start;
+	int ofs_end;
+	int i;
+
+	switch (chan) {
+	case SENSOR_CHAN_ACCEL_X:
+		ofs_start = ofs_end = 0;
+		break;
+	case SENSOR_CHAN_ACCEL_Y:
+		ofs_start = ofs_end = 1;
+		break;
+	case SENSOR_CHAN_ACCEL_Z:
+		ofs_start = ofs_end = 2;
+		break;
+	case SENSOR_CHAN_ACCEL_XYZ:
+		ofs_start = 0;
+		ofs_end = 2;
+		break;
+	case SENSOR_CHAN_AMBIENT_TEMP:
+		lis2dh_channel_get_temp(dev, val);
+		break;
+	default:
+		return -ENOTSUP;
+	}
+	if (chan != SENSOR_CHAN_AMBIENT_TEMP) {
+		for (i = ofs_start; i <= ofs_end; i++, val++) {
+			lis2dh_convert(lis2dh->sample.xyz[i], lis2dh->scale, val);
+		}
+	}
+
+	return 0;
+}
+
+static int lis2dh_sample_fetch(struct device *dev, enum sensor_channel chan)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	size_t i;
+	int status = -ENODATA;
+
+	switch (chan) {
+		case SENSOR_CHAN_ALL:
+		case SENSOR_CHAN_ACCEL_XYZ:
+			/*
+			* since status and all accel data register addresses are consecutive,
+			* a burst read can be used to read all the samples
+			*/
+			status = lis2dh->hw_tf->read_data(dev, LIS2DH_REG_STATUS,
+							lis2dh->sample.raw,
+							sizeof(lis2dh->sample.raw));
+			if (status < 0) {
+				LOG_WRN("Could not read accel axis data");
+				return status;
+			}
+
+			for (i = 0; i < (3 * sizeof(s16_t)); i += sizeof(s16_t)) {
+				s16_t *sample =
+					(s16_t *)&lis2dh->sample.raw[1 + i];
+
+				*sample = sys_le16_to_cpu(*sample);
+			}
+
+			if (lis2dh->sample.status & LIS2DH_STATUS_DRDY_MASK) {
+				return 0;
+			}
+			break;
+		case SENSOR_CHAN_AMBIENT_TEMP:
+			status = lis2dh_sample_fetch_temp(dev);
+			break;
+		default:
+			break;
+	}
+
+	return status;
+}
+
+#ifdef CONFIG_MG100_LIS2DH_ODR_RUNTIME
+/* 1620 & 5376 are low power only */
+static const u16_t lis2dh_odr_map[] = {0, 1, 10, 25, 50, 100, 200, 400, 1620,
+				       1344, 5376};
+
+static int lis2dh_freq_to_odr_val(u16_t freq)
+{
+	size_t i;
+
+	/* An ODR of 0 Hz is not allowed */
+	if (freq == 0U) {
+		return -EINVAL;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(lis2dh_odr_map); i++) {
+		if (freq == lis2dh_odr_map[i]) {
+			return i;
+		}
+	}
+
+	return -EINVAL;
+}
+
+static int lis2dh_acc_odr_set(struct device *dev, u16_t freq)
+{
+	int odr;
+	int status;
+	u8_t value;
+	struct lis2dh_data *data = dev->driver_data;
+
+	odr = lis2dh_freq_to_odr_val(freq);
+	if (odr < 0) {
+		return odr;
+	}
+
+	status = data->hw_tf->read_reg(dev, LIS2DH_REG_CTRL1, &value);
+	if (status < 0) {
+		return status;
+	}
+
+	/* some odr values cannot be set in certain power modes */
+	if ((value & LIS2DH_LP_EN_BIT_MASK) == 0U && odr == LIS2DH_ODR_8) {
+		return -ENOTSUP;
+	}
+
+	/* adjust odr index for LP enabled mode, see table above */
+	if (((value & LIS2DH_LP_EN_BIT_MASK) == LIS2DH_LP_EN_BIT_MASK) &&
+		(odr == LIS2DH_ODR_9 + 1)) {
+		odr--;
+	}
+
+	return data->hw_tf->write_reg(dev, LIS2DH_REG_CTRL1,
+				      (value & ~LIS2DH_ODR_MASK) |
+				      LIS2DH_ODR_RATE(odr));
+}
+#endif
+
+#ifdef CONFIG_MG100_LIS2DH_ACCEL_RANGE_RUNTIME
+
+#define LIS2DH_RANGE_IDX_TO_VALUE(idx)		(1 << ((idx) + 1))
+#define LIS2DH_NUM_RANGES			4
+
+static int lis2dh_range_to_reg_val(u16_t range)
+{
+	int i;
+
+	for (i = 0; i < LIS2DH_NUM_RANGES; i++) {
+		if (range == LIS2DH_RANGE_IDX_TO_VALUE(i)) {
+			return i;
+		}
+	}
+
+	return -EINVAL;
+}
+
+static int lis2dh_acc_range_set(struct device *dev, s32_t range)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	int fs;
+
+	fs = lis2dh_range_to_reg_val(range);
+	if (fs < 0) {
+		return fs;
+	}
+
+	lis2dh->scale = lis2dh_reg_val_to_scale[fs];
+
+	return lis2dh->hw_tf->update_reg(dev, LIS2DH_REG_CTRL4,
+					 LIS2DH_FS_MASK,
+					 (fs << LIS2DH_FS_SHIFT));
+}
+#endif
+
+static int lis2dh_acc_config(struct device *dev, enum sensor_channel chan,
+			    enum sensor_attribute attr,
+			    const struct sensor_value *val)
+{
+	switch (attr) {
+#ifdef CONFIG_MG100_LIS2DH_ACCEL_RANGE_RUNTIME
+	case SENSOR_ATTR_FULL_SCALE:
+		return lis2dh_acc_range_set(dev, sensor_ms2_to_g(val));
+#endif
+#ifdef CONFIG_MG100_LIS2DH_ODR_RUNTIME
+	case SENSOR_ATTR_SAMPLING_FREQUENCY:
+		return lis2dh_acc_odr_set(dev, val->val1);
+#endif
+#if defined(CONFIG_MG100_LIS2DH_TRIGGER)
+	case SENSOR_ATTR_SLOPE_TH:
+	case SENSOR_ATTR_SLOPE_DUR:
+		return lis2dh_acc_slope_config(dev, attr, val);
+#endif
+	default:
+		LOG_DBG("Accel attribute not supported.");
+		return -ENOTSUP;
+	}
+
+	return 0;
+}
+
+static int lis2dh_attr_set(struct device *dev, enum sensor_channel chan,
+			   enum sensor_attribute attr,
+			   const struct sensor_value *val)
+{
+	switch (chan) {
+	case SENSOR_CHAN_ACCEL_X:
+	case SENSOR_CHAN_ACCEL_Y:
+	case SENSOR_CHAN_ACCEL_Z:
+	case SENSOR_CHAN_ACCEL_XYZ:
+		return lis2dh_acc_config(dev, chan, attr, val);
+	default:
+		LOG_WRN("attr_set() not supported on this channel.");
+		return -ENOTSUP;
+	}
+
+	return 0;
+}
+
+static const struct sensor_driver_api lis2dh_driver_api = {
+	.attr_set = lis2dh_attr_set,
+#if CONFIG_MG100_LIS2DH_TRIGGER
+	.trigger_set = lis2dh_trigger_set,
+#endif
+	.sample_fetch = lis2dh_sample_fetch,
+	.channel_get = lis2dh_channel_get,
+};
+
+int lis2dh_init(struct device *dev)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	const struct lis2dh_config *cfg = dev->config_info;
+	int status;
+	u8_t id;
+	u8_t raw[6];
+
+	lis2dh->bus = device_get_binding(cfg->bus_name);
+	if (!lis2dh->bus) {
+		LOG_ERR("master not found: %s", cfg->bus_name);
+		return -EINVAL;
+	}
+
+	cfg->bus_init(dev);
+
+	status = lis2dh->hw_tf->read_reg(dev, LIS2DH_REG_WAI, &id);
+	if (status < 0) {
+		LOG_WRN("Failed to read chip id.");
+		return status;
+	}
+
+	if (id != LIS2DH_CHIP_ID) {
+		LOG_WRN("Invalid chip ID: %02x\n", id);
+		return -EINVAL;
+	}
+
+	if (IS_ENABLED(DT_INST_PROP(0, disconnect_sdo_sa0_pull_up))) {
+		status = lis2dh->hw_tf->update_reg(dev, LIS2DH_REG_CTRL0,
+						   LIS2DH_SDO_PU_DISC_MASK,
+						   LIS2DH_SDO_PU_DISC_MASK);
+		if (status < 0) {
+			LOG_ERR("Failed to disconnect SDO/SA0 pull-up.");
+			return status;
+		}
+	}
+
+	/* Initialize control register ctrl1 to ctrl 6 to default boot values
+	 * to avoid warm start/reset issues as the accelerometer has no reset
+	 * pin. Register values are retained if power is not removed.
+	 * Default values see LIS2DH documentation page 30, chapter 6.
+	 */
+	(void)memset(raw, 0, sizeof(raw));
+	raw[0] = LIS2DH_ACCEL_EN_BITS;
+
+	status = lis2dh->hw_tf->write_data(dev, LIS2DH_REG_CTRL1, raw,
+					   sizeof(raw));
+
+	if (status < 0) {
+		LOG_ERR("Failed to reset ctrl registers.");
+		return status;
+	}
+
+	/* store the full scale range for conversion later */
+	lis2dh->scale = lis2dh_reg_val_to_scale[LIS2DH_FS_IDX];
+	/* set the full scale range, high resolution, and block data update settings. */
+	status = lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_CTRL4,
+					  LIS2DH_FS_BITS | LIS2DH_HR_BIT | LIS2DH_CTRL4_BDU_BIT);
+
+	/*
+	 *  on the LIS2DH/LIS3DH, ADC3 is used for reading the temperature values.
+	 *  Both ADC and Temperature measurements must be enabled.
+	 */
+	status = lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_TEMP_CFG_REG,
+						LIS2DH_TEMP_CFG_EN_BITS);
+
+	if (status < 0) {
+		LOG_ERR("Failed to set full scale ctrl register.");
+		return status;
+	}
+
+	LOG_INF("bus=%s fs=%d, odr=0x%x lp_en=0x%x scale=%d",
+		    LIS2DH_BUS_DEV_NAME, 1 << (LIS2DH_FS_IDX + 1),
+		    LIS2DH_ODR_IDX, (u8_t)LIS2DH_LP_EN_BIT, lis2dh->scale);
+
+	/* enable accel measurements and set power mode and data rate */
+	return lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_CTRL1,
+					LIS2DH_ACCEL_EN_BITS | LIS2DH_LP_EN_BIT |
+					LIS2DH_ODR_BITS);
+}
+
+static struct lis2dh_data lis2dh_data;
+
+static const struct lis2dh_config lis2dh_config = {
+	.bus_name = DT_INST_BUS_LABEL(0),
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
+	.bus_init = lis2dh_spi_init,
+	.spi_conf.frequency = DT_INST_PROP(0, spi_max_frequency),
+	.spi_conf.operation = (SPI_OP_MODE_MASTER | SPI_MODE_CPOL |
+			       SPI_MODE_CPHA | SPI_WORD_SET(8) |
+			       SPI_LINES_SINGLE),
+	.spi_conf.slave     = DT_INST_REG_ADDR(0),
+#if DT_INST_SPI_DEV_HAS_CS_GPIOS(0)
+	.gpio_cs_port	    = DT_INST_SPI_DEV_CS_GPIOS_LABEL(0),
+	.cs_gpio	    = DT_INST_SPI_DEV_CS_GPIOS_PIN(0),
+	.spi_conf.cs        =  &lis2dh_data.cs_ctrl,
+#else
+	.spi_conf.cs        = NULL,
+#endif
+#elif DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
+	.bus_init = lis2dh_i2c_init,
+	.i2c_slv_addr = DT_INST_REG_ADDR(0),
+#else
+#error "BUS MACRO NOT DEFINED IN DTS"
+#endif
+};
+
+DEVICE_AND_API_INIT(lis2dh, DT_INST_LABEL(0), lis2dh_init, &lis2dh_data,
+		    &lis2dh_config, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,
+		    &lis2dh_driver_api);
---
+++ drivers/mg100_lis2dh/mg100_lis2dh_trigger.c
@@ -0,0 +1,355 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT st_lis2dh
+
+#include <sys/util.h>
+#include <kernel.h>
+#include <logging/log.h>
+
+#define START_TRIG_INT1			0
+#define TRIGGED_INT1			4
+
+LOG_MODULE_DECLARE(lis2dh, CONFIG_SENSOR_LOG_LEVEL);
+#include "mg100_lis2dh.h"
+
+static inline void setup_int1(struct device *dev,
+			      bool enable)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+
+	gpio_pin_interrupt_configure(lis2dh->gpio_int1,
+				     LIS2DH_INT1_GPIOS_PIN,
+				     enable
+				     ? GPIO_INT_EDGE_TO_ACTIVE
+				     : GPIO_INT_DISABLE);
+}
+
+static int lis2dh_trigger_drdy_set(struct device *dev, enum sensor_channel chan,
+				   sensor_trigger_handler_t handler)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	int status;
+
+	setup_int1(dev, false);
+
+	/* cancel potentially pending trigger */
+	atomic_clear_bit(&lis2dh->trig_flags, TRIGGED_INT1);
+
+	status = lis2dh->hw_tf->update_reg(dev, LIS2DH_REG_CTRL3,
+					   LIS2DH_EN_DRDY1_INT1, 0);
+
+	lis2dh->handler_drdy = handler;
+	if ((handler == NULL) || (status < 0)) {
+		return status;
+	}
+
+	lis2dh->chan_drdy = chan;
+
+	/* serialize start of int1 in thread to synchronize output sampling
+	 * and first interrupt. this avoids concurrent bus context access.
+	 */
+	atomic_set_bit(&lis2dh->trig_flags, START_TRIG_INT1);
+#if defined(CONFIG_MG100_LIS2DH_TRIGGER_OWN_THREAD)
+	k_sem_give(&lis2dh->gpio_sem);
+#elif defined(CONFIG_MG100_LIS2DH_TRIGGER_GLOBAL_THREAD)
+	k_work_submit(&lis2dh->work);
+#endif
+	return 0;
+}
+
+#define LIS2DH_ANYM_CFG (LIS2DH_INT_CFG_ZHIE_ZUPE | LIS2DH_INT_CFG_YHIE_YUPE |\
+			 LIS2DH_INT_CFG_XHIE_XUPE)
+
+static int lis2dh_trigger_anym_set(struct device *dev,
+				   sensor_trigger_handler_t handler)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	int status;
+	u8_t reg_val;
+
+	status = lis2dh_init_interrupt(dev);
+	if (status < 0) {
+		LOG_ERR("Failed to initialize interrupts.");
+		return status;
+	}
+
+	setup_int1(dev, true);
+
+	/* cancel potentially pending trigger */
+	atomic_clear_bit(&lis2dh->trig_flags, TRIGGED_INT1);
+
+	status = lis2dh->hw_tf->update_reg(dev, LIS2DH_REG_CTRL3,
+					   LIS2DH_EN_DRDY1_INT1, 0);
+
+	/* disable all interrupt 1 events */
+	status = lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_INT1_CFG, 0);
+
+	/* make sure any pending interrupt is cleared */
+	status = lis2dh->hw_tf->read_reg(dev, LIS2DH_REG_INT1_SRC, &reg_val);
+
+	lis2dh->handler_anymotion = handler;
+	if ((handler == NULL) || (status < 0)) {
+		return status;
+	}
+
+	/* serialize start of int1 in thread to synchronize output sampling
+	 * and first interrupt. this avoids concurrent bus context access.
+	 */
+	atomic_set_bit(&lis2dh->trig_flags, START_TRIG_INT1);
+#if defined(CONFIG_MG100_LIS2DH_TRIGGER_OWN_THREAD)
+	k_sem_give(&lis2dh->gpio_sem);
+#elif defined(CONFIG_MG100_LIS2DH_TRIGGER_GLOBAL_THREAD)
+	k_work_submit(&lis2dh->work);
+#endif
+	return 0;
+}
+
+static int lis2dh_start_trigger_int1(struct device *dev)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+
+	setup_int1(dev, true);
+
+	return lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_INT1_CFG,
+					LIS2DH_ANYM_CFG);
+}
+
+int lis2dh_trigger_set(struct device *dev,
+		       const struct sensor_trigger *trig,
+		       sensor_trigger_handler_t handler)
+{
+	if (trig->type == SENSOR_TRIG_DATA_READY &&
+	    trig->chan == SENSOR_CHAN_ACCEL_XYZ) {
+		return lis2dh_trigger_drdy_set(dev, trig->chan, handler);
+	} else if (trig->type == SENSOR_TRIG_DELTA) {
+		return lis2dh_trigger_anym_set(dev, handler);
+	}
+
+	return -ENOTSUP;
+}
+
+int lis2dh_acc_slope_config(struct device *dev, enum sensor_attribute attr,
+			    const struct sensor_value *val)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	int status;
+
+	if (attr == SENSOR_ATTR_SLOPE_TH) {
+		u8_t range_g, reg_val;
+		u32_t slope_th_ums2;
+
+		status = lis2dh->hw_tf->read_reg(dev, LIS2DH_REG_CTRL4,
+						 &reg_val);
+		if (status < 0) {
+			return status;
+		}
+
+		/* fs reg value is in the range 0 (2g) - 3 (16g) */
+		range_g = 2 * (1 << ((LIS2DH_FS_MASK & reg_val)
+				      >> LIS2DH_FS_SHIFT));
+
+		slope_th_ums2 = val->val1 * 1000000 + val->val2;
+
+		/* make sure the provided threshold does not exceed range */
+		if ((slope_th_ums2 - 1) > (range_g * SENSOR_G)) {
+			return -EINVAL;
+		}
+
+		/* 7 bit full range value */
+		reg_val = 128 / range_g * (slope_th_ums2 - 1) / SENSOR_G;
+
+		LOG_INF("int1_ths=0x%x range_g=%d ums2=%u", reg_val,
+			    range_g, slope_th_ums2 - 1);
+
+		status = lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_INT1_THS,
+						  reg_val);
+	} else { /* SENSOR_ATTR_SLOPE_DUR */
+		/*
+		 * slope duration is measured in number of samples:
+		 * N/ODR where N is the register value
+		 */
+		if (val->val1 < 0 || val->val1 > 127) {
+			return -ENOTSUP;
+		}
+
+		LOG_INF("int1_dur=0x%x", val->val1);
+
+		status = lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_INT1_DUR,
+						  val->val1);
+	}
+
+	return status;
+}
+
+static void lis2dh_gpio_int1_callback(struct device *dev,
+				      struct gpio_callback *cb, u32_t pins)
+{
+	struct lis2dh_data *lis2dh =
+		CONTAINER_OF(cb, struct lis2dh_data, gpio_int1_cb);
+
+	ARG_UNUSED(pins);
+
+	atomic_set_bit(&lis2dh->trig_flags, TRIGGED_INT1);
+
+#if defined(CONFIG_MG100_LIS2DH_TRIGGER_OWN_THREAD)
+	k_sem_give(&lis2dh->gpio_sem);
+#elif defined(CONFIG_MG100_LIS2DH_TRIGGER_GLOBAL_THREAD)
+	k_work_submit(&lis2dh->work);
+#endif
+}
+
+static void lis2dh_thread_cb(void *arg)
+{
+	struct device *dev = arg;
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	int status;
+	u8_t reg_val;
+
+
+	if (unlikely(atomic_test_and_clear_bit(&lis2dh->trig_flags,
+		     START_TRIG_INT1))) {
+		status = lis2dh_start_trigger_int1(dev);
+
+		if (unlikely(status < 0)) {
+			LOG_ERR("lis2dh_start_trigger_int1: %d", status);
+		}
+		return;
+	}
+
+	if (atomic_test_and_clear_bit(&lis2dh->trig_flags,
+				      TRIGGED_INT1)) {
+		struct sensor_trigger anym_trigger = {
+			.type = SENSOR_TRIG_DELTA,
+			.chan = lis2dh->chan_drdy,
+		};
+
+		/* clear interrupt 1 to de-assert int1 line */
+		status = lis2dh->hw_tf->read_reg(dev, LIS2DH_REG_INT1_SRC,
+						 &reg_val);
+
+		if (status < 0) {
+			LOG_ERR("clearing interrupt 1 failed: %d", status);
+			return;
+		}
+
+		if (likely(lis2dh->handler_anymotion != NULL)) {
+			lis2dh->handler_anymotion(dev, &anym_trigger);
+		}
+
+		LOG_DBG("@tick=%u int1_src=0x%x", k_cycle_get_32(),
+			    reg_val);
+
+		return;
+	}
+}
+
+#ifdef CONFIG_MG100_LIS2DH_TRIGGER_OWN_THREAD
+static void lis2dh_thread(void *arg1, void *unused2, void *unused3)
+{
+	struct device *dev = arg1;
+	struct lis2dh_data *lis2dh = dev->driver_data;
+
+	ARG_UNUSED(unused2);
+	ARG_UNUSED(unused3);
+
+	while (1) {
+		k_sem_take(&lis2dh->gpio_sem, K_FOREVER);
+		lis2dh_thread_cb(dev);
+	}
+}
+#endif
+
+#ifdef CONFIG_MG100_LIS2DH_TRIGGER_GLOBAL_THREAD
+static void lis2dh_work_cb(struct k_work *work)
+{
+	struct lis2dh_data *lis2dh =
+		CONTAINER_OF(work, struct lis2dh_data, work);
+
+	lis2dh_thread_cb(lis2dh->dev);
+}
+#endif
+
+int lis2dh_init_interrupt(struct device *dev)
+{
+	struct lis2dh_data *lis2dh = dev->driver_data;
+	int status;
+	u8_t raw[2];
+
+	/* setup data ready gpio interrupt */
+	lis2dh->gpio_int1 = device_get_binding(LIS2DH_INT1_GPIO_DEV_NAME);
+	if (lis2dh->gpio_int1 == NULL) {
+		LOG_ERR("Cannot get pointer to %s device",
+			    LIS2DH_INT1_GPIO_DEV_NAME);
+		return -EINVAL;
+	}
+
+#if defined(CONFIG_MG100_LIS2DH_TRIGGER_OWN_THREAD)
+	k_sem_init(&lis2dh->gpio_sem, 0, UINT_MAX);
+
+	k_thread_create(&lis2dh->thread, lis2dh->thread_stack,
+			CONFIG_MG100_LIS2DH_THREAD_STACK_SIZE,
+			(k_thread_entry_t)lis2dh_thread, dev, NULL, NULL,
+			K_PRIO_COOP(CONFIG_MG100_LIS2DH_THREAD_PRIORITY), 0,
+			K_NO_WAIT);
+#elif defined(CONFIG_MG100_LIS2DH_TRIGGER_GLOBAL_THREAD)
+	lis2dh->work.handler = lis2dh_work_cb;
+	lis2dh->dev = dev;
+#endif
+
+	/* any motion int1 gpio configuration */
+	status = gpio_pin_configure(lis2dh->gpio_int1, LIS2DH_INT1_GPIOS_PIN,
+				    GPIO_INPUT | LIS2DH_INT1_GPIOS_FLAGS);
+	if (status < 0) {
+		LOG_ERR("Could not configure gpio %d",
+			     LIS2DH_INT1_GPIOS_PIN);
+		return status;
+	}
+
+	gpio_init_callback(&lis2dh->gpio_int1_cb,
+			   lis2dh_gpio_int1_callback,
+			   BIT(LIS2DH_INT1_GPIOS_PIN));
+
+	/* callback is going to be enabled by trigger setting function */
+	status = gpio_add_callback(lis2dh->gpio_int1, &lis2dh->gpio_int1_cb);
+	if (status < 0) {
+		LOG_ERR("Could not add gpio int1 callback (%d)", status);
+		return status;
+	}
+
+	LOG_INF("int1 on pin=%d", LIS2DH_INT1_GPIOS_PIN);
+
+	/* disable interrupt 1 in case of warm (re)boot */
+	status = lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_INT1_CFG, 0);
+	if (status < 0) {
+		LOG_ERR("Interrupt 1 disable reg write failed (%d)",
+			    status);
+		return status;
+	}
+
+	(void)memset(raw, 0, sizeof(raw));
+	status = lis2dh->hw_tf->write_data(dev, LIS2DH_REG_INT1_THS,
+					   raw, sizeof(raw));
+	if (status < 0) {
+		LOG_ERR("Burst write to INT1 THS failed (%d)", status);
+		return status;
+	}
+
+	/* enable interrupt 1 on int1 line */
+	status = lis2dh->hw_tf->update_reg(dev, LIS2DH_REG_CTRL3,
+					   LIS2DH_EN_INT1_INT1,
+					   LIS2DH_EN_INT1_INT1);
+
+	/* latch int1 line interrupt */
+	status = lis2dh->hw_tf->write_reg(dev, LIS2DH_REG_CTRL5,
+					  LIS2DH_EN_LIR_INT1);
+	if (status < 0) {
+		LOG_ERR("INT1 latch enable reg write failed (%d)", status);
+		return status;
+	}
+
+	return status;
+}
