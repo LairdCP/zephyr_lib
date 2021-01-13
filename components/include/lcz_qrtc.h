9ae8f25d03ffa6878db9005667b99c7aa61ee65b
Andrew Hedin <andrew.hedin@lairdconnect.com>
2021-01-13 12:10:39 -0600

    Bug 18155 - CT integration - Add minimum value check for epoch
    Add init.
    Add qrtc sync.
    Change format to snake_case.
    Change names to use lcz_.
    Rename file.

--- components/include/lcz_qrtc.h
+++ components/include/lcz_qrtc.h
@@ -1,8 +1,8 @@
 /**
- * @file qrtc.h
+ * @file lcz_qrtc.h
  * @brief Quasi Real Time Clock that uses offset and system ticks.
  *
- * Copyright (c) 2020 Laird Connectivity
+ * Copyright (c) 2021 Laird Connectivity
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -23,24 +23,26 @@ extern "C" {
 /* Global Function Prototypes                                                 */
 /******************************************************************************/
 /**
- * @brief Resets epoch to zero and sets epochWasSet to false.
+ * @brief Resets epoch to 0 and changes epoch_was_set to false.
+ *
+ * @note Called by sys init.
  */
-void Qrtc_Init(void);
+void lcz_qrtc_init(void);
 
 /**
  * @brief Set the epoch
  *
- * @param Epoch in seconds from Jan 1, 1970
+ * @param epoch in seconds from Jan 1, 1970
  *
  * @retval recomputed value for testing
  */
-uint32_t Qrtc_SetEpoch(uint32_t Epoch);
+uint32_t lcz_qrtc_set_epoch(uint32_t epoch);
 
 /**
  * @brief Set the epoch using time structure.
  *
  * @param pTm is a pointer to a time structure
- * @param OffsetSeconds is the offset in seconds from UTC that the time structure
+ * @param offset_seconds is the offset in seconds from UTC that the time structure
  * contains.
  *
  * @note The cellular modem provides local time and an offset and it is much
@@ -48,17 +50,22 @@ uint32_t Qrtc_SetEpoch(uint32_t Epoch);
  *
  * @retval epoch for testing
  */
-uint32_t Qrtc_SetEpochFromTm(struct tm *pTm, int32_t OffsetSeconds);
+uint32_t lcz_qrtc_set_epoch_from_tm(struct tm *pTm, int32_t offset_seconds);
 
 /**
  * @retval Seconds since Jan 1, 1970.
  */
-uint32_t Qrtc_GetEpoch(void);
+uint32_t lcz_qrtc_get_epoch(void);
 
 /**
  * @retval true if the epoch has been set, otherwise false
  */
-bool Qrtc_EpochWasSet(void);
+bool lcz_qrtc_epoch_was_set(void);
+
+/**
+ * @brief Override weak implementation in application.
+ */
+void lcz_qrtc_sync_handler(void);
 
 #ifdef __cplusplus
 }
---
+++ components/source/lcz_qrtc.c
@@ -0,0 +1,159 @@
+/**
+ * @file lcz_qrtc.c
+ * @brief
+ *
+ * Copyright (c) 2021 Laird Connectivity
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <logging/log.h>
+LOG_MODULE_REGISTER(lcz_qrtc, CONFIG_LCZ_QRTC_LOG_LEVEL);
+
+/******************************************************************************/
+/* Includes                                                                   */
+/******************************************************************************/
+#include <init.h>
+#include <zephyr.h>
+#include <stdlib.h>
+
+#include "lcz_qrtc.h"
+
+/******************************************************************************/
+/* Local Constant, Macro and Type Definitions                                 */
+/******************************************************************************/
+struct qrtc {
+	bool epoch_was_set;
+	uint32_t offset;
+	struct k_mutex mutex;
+	struct k_delayed_work work;
+};
+
+/******************************************************************************/
+/* Local Data Definitions                                                     */
+/******************************************************************************/
+static struct qrtc qrtc;
+
+/******************************************************************************/
+/* Local Function Prototypes                                                  */
+/******************************************************************************/
+static int qrtc_sys_init(const struct device *device);
+static void update_offset(uint32_t epoch);
+static uint32_t get_uptime_seconds(void);
+static uint32_t convert_time_to_epoch(time_t Time);
+static void qrtc_sync_handler(struct k_work *dummy);
+
+/******************************************************************************/
+/* Global Function Definitions                                                */
+/******************************************************************************/
+SYS_INIT(qrtc_sys_init, POST_KERNEL, CONFIG_LCZ_QRTC_INIT_PRIORITY);
+
+void lcz_qrtc_init(void)
+{
+	k_mutex_init(&qrtc.mutex);
+	qrtc.offset = 0;
+	qrtc.epoch_was_set = false;
+	k_delayed_work_init(&qrtc.work, qrtc_sync_handler);
+
+#if CONFIG_LCZ_QRTC_SYNC_INTERVAL_SECONDS != 0
+	k_delayed_work_submit(&qrtc.work,
+			      K_SECONDS(CONFIG_LCZ_QRTC_SYNC_INTERVAL_SECONDS));
+#endif
+}
+
+uint32_t lcz_qrtc_set_epoch(uint32_t epoch)
+{
+	update_offset(epoch);
+	return lcz_qrtc_get_epoch();
+}
+
+uint32_t lcz_qrtc_set_epoch_from_tm(struct tm *pTm, int32_t offset_seconds)
+{
+	time_t rawTime = mktime(pTm);
+	uint32_t epoch = convert_time_to_epoch(rawTime);
+
+	/* (local + offset) = UTC */
+	if (abs(offset_seconds) < epoch) {
+		epoch -= offset_seconds;
+		update_offset(epoch);
+	}
+	return lcz_qrtc_get_epoch();
+}
+
+uint32_t lcz_qrtc_get_epoch(void)
+{
+	return (get_uptime_seconds() + qrtc.offset);
+}
+
+bool lcz_qrtc_epoch_was_set(void)
+{
+	return qrtc.epoch_was_set;
+}
+
+/******************************************************************************/
+/* Local Function Definitions                                                 */
+/******************************************************************************/
+static int qrtc_sys_init(const struct device *device)
+{
+	ARG_UNUSED(device);
+
+	lcz_qrtc_init();
+
+	return 0;
+}
+
+/**
+ * @brief Generate and save an offset using the current uptime.
+ * This is a quasi-RTC because it isn't battery backed or temperature compensated.
+ * Its resolution depends on k_uptime_get.
+ */
+static void update_offset(uint32_t epoch)
+{
+	if (epoch < CONFIG_LCZ_QRTC_MINIMUM_EPOCH) {
+		LOG_ERR("Invalid epoch time - QRTC offset not updated");
+	} else {
+		k_mutex_lock(&qrtc.mutex, K_FOREVER);
+		uint32_t uptime = get_uptime_seconds();
+		if (epoch >= uptime) {
+			qrtc.offset = epoch - uptime;
+			qrtc.epoch_was_set = true;
+		}
+		k_mutex_unlock(&qrtc.mutex);
+	}
+}
+
+static uint32_t get_uptime_seconds(void)
+{
+	int64_t uptimeMs = k_uptime_get();
+
+	if (uptimeMs < 0) {
+		return 0;
+	}
+	return (uint32_t)(uptimeMs / MSEC_PER_SEC);
+}
+
+static uint32_t convert_time_to_epoch(time_t Time)
+{
+	/* Time is a long long int in Zephyr. */
+	if (Time < 0) {
+		return 0;
+	} else if (Time >= UINT32_MAX) {
+		return 0;
+	} else {
+		return (uint32_t)Time;
+	}
+}
+
+static void qrtc_sync_handler(struct k_work *dummy)
+{
+	ARG_UNUSED(dummy);
+
+	lcz_qrtc_sync_handler();
+
+	k_delayed_work_submit(&qrtc.work,
+			      K_SECONDS(CONFIG_LCZ_QRTC_SYNC_INTERVAL_SECONDS));
+}
+
+__weak void lcz_qrtc_sync_handler(void)
+{
+	return;
+}
