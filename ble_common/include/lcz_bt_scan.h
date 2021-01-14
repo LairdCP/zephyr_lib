cbe1dd27939aac62d69b82603d18068d1469ee66
Andrew Hedin <andrew.hedin@lairdconnect.com>
2021-01-14 08:50:39 -0600

    Bug 18155 - CT integration - Add number of scan start/stops and is scanning.
    Add config for log level.
    Change names to use lcz_.
    Move scan variables into struct for easier debug.

--- ble_common/include/lcz_bt_scan.h
+++ ble_common/include/lcz_bt_scan.h
@@ -1,16 +1,16 @@
 /**
- * @file bt_scan.h
+ * @file lcz_bt_scan.h
  * @brief Controls Bluetooth scanning for multiple centrals/observers with a
  * counting semaphore.
  *
  * @note Scanning must be disabled in order to connect.
  *
- * Copyright (c) 2020 Laird Connectivity
+ * Copyright (c) 2021 Laird Connectivity
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef __BT_SCAN_H__
-#define __BT_SCAN_H__
+#ifndef __LCZ_BT_SCAN_H__
+#define __LCZ_BT_SCAN_H__
 
 /******************************************************************************/
 /* Includes                                                                   */
@@ -33,7 +33,7 @@ extern "C" {
  *
  * @retval true if success, false otherwise
  */
-bool bt_scan_set_parameters(const struct bt_le_scan_param *param);
+bool lcz_bt_scan_set_parameters(const struct bt_le_scan_param *param);
 
 /**
  * @brief Register user of scan module.
@@ -43,33 +43,54 @@ bool bt_scan_set_parameters(const struct bt_le_scan_param *param);
  *
  * @retval true if new user was registered, false otherwise.
  */
-bool bt_scan_register(int *pId, bt_le_scan_cb_t *cb);
+bool lcz_bt_scan_register(int *pId, bt_le_scan_cb_t *cb);
 
 /**
  * @brief Start scanning (if there aren't any stop requests).
  */
-void bt_scan_start(int id);
+void lcz_bt_scan_start(int id);
 
 /**
  * @brief Stop scanning.
  */
-void bt_scan_stop(int id);
+void lcz_bt_scan_stop(int id);
 
 /**
  * @brief Clear stop request.
  * Restart scanning if there aren't any stop requests and there is at least
  * one start request.
  */
-void bt_scan_resume(int id);
+void lcz_bt_scan_resume(int id);
 
 /**
  * @brief Clear stop request, set start request, and start scanning
  * if there aren't any other stop requests.
  */
-void bt_scan_restart(int id);
+void lcz_bt_scan_restart(int id);
+
+/**
+ * @brief Accessor function
+ *
+ * @retval true if scanning, false otherwise
+ */
+bool lcz_bt_scan_active(void);
+
+/**
+ * @brief Accessor function
+ *
+ * @retval number of scan starts (!= the number of scan start requests)
+ */
+uint32_t lcz_bt_scan_get_num_starts(void);
+
+/**
+ * @brief Accessor function
+ *
+ * @retval number of scan stops
+ */
+uint32_t lcz_bt_scan_get_num_stops(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __BT_SCAN_H__ */
+#endif /* __LCZ_BT_SCAN_H__ */
--- ble_common/source/lcz_bt_scan.c
+++ ble_common/source/lcz_bt_scan.c
@@ -1,39 +1,44 @@
 /**
- * @file bt_scan.c
+ * @file lcz_bt_scan.c
  * @brief
  *
- * Copyright (c) 2020 Laird Connectivity
+ * Copyright (c) 2021 Laird Connectivity
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <logging/log.h>
-#define LOG_LEVEL LOG_LEVEL_DBG
-LOG_MODULE_REGISTER(bt_scan);
+LOG_MODULE_REGISTER(lcz_bt_scan, CONFIG_LCZ_BT_SCAN_LOG_LEVEL);
 
 /******************************************************************************/
 /* Includes                                                                   */
 /******************************************************************************/
 #include <kernel.h>
 #include <stddef.h>
-#include "bt_scan.h"
+
+#include "lcz_bt_scan.h"
 
 /******************************************************************************/
 /* Local Function Prototypes                                                  */
 /******************************************************************************/
 static void scan_start(void);
 static bool valid_user_id(int id);
-static void bt_scan_adv_handler(const bt_addr_le_t *addr, int8_t rssi,
-				uint8_t type, struct net_buf_simple *ad);
+static void lcz_bt_scan_adv_handler(const bt_addr_le_t *addr, int8_t rssi,
+				    uint8_t type, struct net_buf_simple *ad);
 
 /******************************************************************************/
 /* Local Data Definitions                                                     */
 /******************************************************************************/
-static atomic_t scanning = ATOMIC_INIT(0);
-static atomic_t bts_users = ATOMIC_INIT(0);
-static atomic_t stop_requests = ATOMIC_INIT(0);
-static atomic_t start_requests = ATOMIC_INIT(0);
-static bt_le_scan_cb_t *adv_handlers[CONFIG_BT_SCAN_MAX_USERS];
+static struct {
+	atomic_t scanning;
+	atomic_t users;
+	atomic_t stop_requests;
+	atomic_t start_requests;
+	bt_le_scan_cb_t *adv_handlers[CONFIG_LCZ_BT_SCAN_MAX_USERS];
+
+	uint32_t num_stops;
+	uint32_t num_starts;
+} bts;
 
 static struct bt_le_scan_param scan_parameters = BT_LE_SCAN_PARAM_INIT(
 	BT_LE_SCAN_TYPE_PASSIVE, BT_LE_SCAN_OPT_FILTER_DUPLICATE,
@@ -42,9 +47,9 @@ static struct bt_le_scan_param scan_parameters = BT_LE_SCAN_PARAM_INIT(
 /******************************************************************************/
 /* Global Function Definitions                                                */
 /******************************************************************************/
-bool bt_scan_set_parameters(const struct bt_le_scan_param *param)
+bool lcz_bt_scan_set_parameters(const struct bt_le_scan_param *param)
 {
-	if (atomic_get(&bts_users) != 0) {
+	if (atomic_get(&bts.users) != 0) {
 		return false;
 	} else {
 		memcpy(&scan_parameters, param, sizeof(scan_parameters));
@@ -52,52 +57,71 @@ bool bt_scan_set_parameters(const struct bt_le_scan_param *param)
 	}
 }
 
-bool bt_scan_register(int *pId, bt_le_scan_cb_t *cb)
+bool lcz_bt_scan_register(int *pId, bt_le_scan_cb_t *cb)
 {
-	*pId = (int)atomic_inc(&bts_users);
+	*pId = (int)atomic_inc(&bts.users);
+
 	if (valid_user_id(*pId)) {
-		adv_handlers[*pId] = cb;
+		bts.adv_handlers[*pId] = cb;
 		return true;
 	} else {
 		return false;
 	}
 }
 
-void bt_scan_start(int id)
+void lcz_bt_scan_start(int id)
 {
 	if (valid_user_id(id)) {
-		atomic_set_bit(&start_requests, id);
+		atomic_set_bit(&bts.start_requests, id);
 		scan_start();
 	}
 }
 
-void bt_scan_stop(int id)
+void lcz_bt_scan_stop(int id)
 {
 	if (valid_user_id(id)) {
-		atomic_clear_bit(&start_requests, id);
-		atomic_set_bit(&stop_requests, id);
-		if (atomic_cas(&scanning, 1, 0)) {
+		atomic_clear_bit(&bts.start_requests, id);
+		atomic_set_bit(&bts.stop_requests, id);
+		if (atomic_cas(&bts.scanning, 1, 0)) {
 			int err = bt_le_scan_stop();
 			LOG_DBG("%d", err);
+			if (err == 0) {
+				bts.num_stops += 1;
+			}
 		}
 	}
 }
 
-void bt_scan_resume(int id)
+void lcz_bt_scan_resume(int id)
 {
 	if (valid_user_id(id)) {
-		atomic_clear_bit(&stop_requests, id);
+		atomic_clear_bit(&bts.stop_requests, id);
 		scan_start();
 	}
 }
 
-void bt_scan_restart(int id)
+void lcz_bt_scan_restart(int id)
 {
 	if (valid_user_id(id)) {
-		atomic_clear_bit(&stop_requests, id);
-		atomic_set_bit(&start_requests, id);
+		atomic_clear_bit(&bts.stop_requests, id);
+		atomic_set_bit(&bts.start_requests, id);
 		scan_start();
 	}
+}
+
+bool lcz_bt_scan_active(void)
+{
+	return (atomic_get(&bts.scanning) != 0);
+}
+
+uint32_t lcz_bt_scan_get_num_starts(void)
+{
+	return bts.num_starts;
+}
+
+uint32_t lcz_bt_scan_get_num_stops(void)
+{
+	return bts.num_stops;
 }
 
 /******************************************************************************/
@@ -105,27 +129,29 @@ void bt_scan_restart(int id)
 /******************************************************************************/
 static void scan_start(void)
 {
-	if (atomic_get(&start_requests) == 0) {
+	if (atomic_get(&bts.start_requests) == 0) {
 		return;
 	}
 
-	if (atomic_get(&stop_requests) != 0) {
+	if (atomic_get(&bts.stop_requests) != 0) {
 		return;
 	}
 
-	if (atomic_cas(&scanning, 0, 1)) {
-		int err =
-			bt_le_scan_start(&scan_parameters, bt_scan_adv_handler);
+	if (atomic_cas(&bts.scanning, 0, 1)) {
+		int err = bt_le_scan_start(&scan_parameters,
+					   lcz_bt_scan_adv_handler);
 		LOG_DBG("%d", err);
 		if (err != 0) {
-			atomic_clear(&scanning);
+			atomic_clear(&bts.scanning);
+		} else {
+			bts.num_starts += 1;
 		}
 	}
 }
 
 static bool valid_user_id(int id)
 {
-	if (id < CONFIG_BT_SCAN_MAX_USERS) {
+	if (id < CONFIG_LCZ_BT_SCAN_MAX_USERS) {
 		return true;
 	} else {
 		LOG_ERR("Invalid bt scan user id");
@@ -133,10 +159,10 @@ static bool valid_user_id(int id)
 	}
 }
 
-static void bt_scan_adv_handler(const bt_addr_le_t *addr, int8_t rssi,
-				uint8_t type, struct net_buf_simple *ad)
+static void lcz_bt_scan_adv_handler(const bt_addr_le_t *addr, int8_t rssi,
+				    uint8_t type, struct net_buf_simple *ad)
 {
-#if CONFIG_BT_SCAN_VERBOSE_ADV_HANDLER
+#if CONFIG_LCZ_BT_SCAN_VERBOSE_ADV_HANDLER
 	char bt_addr[BT_ADDR_LE_STR_LEN];
 	memset(bt_addr, 0, BT_ADDR_LE_STR_LEN);
 	bt_addr_le_to_str(addr, bt_addr, BT_ADDR_LE_STR_LEN);
@@ -145,9 +171,9 @@ static void bt_scan_adv_handler(const bt_addr_le_t *addr, int8_t rssi,
 #endif
 
 	size_t i;
-	for (i = 0; i < CONFIG_BT_SCAN_MAX_USERS; i++) {
-		if (adv_handlers[i] != NULL) {
-			adv_handlers[i](addr, rssi, type, ad);
+	for (i = 0; i < CONFIG_LCZ_BT_SCAN_MAX_USERS; i++) {
+		if (bts.adv_handlers[i] != NULL) {
+			bts.adv_handlers[i](addr, rssi, type, ad);
 		}
 	}
 }
