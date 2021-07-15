0d2e049631b0459edcfff0c1e631b3af000eb1c1
Andrew Hedin <andrew.hedin@lairdconnect.com>
2021-07-15 15:55:35 -0500

    Improve scanning log messages

--- ble_common/source/lcz_bt_scan.c
+++ ble_common/source/lcz_bt_scan.c
@@ -140,10 +140,11 @@ static void scan_start(void)
 	if (atomic_cas(&bts.scanning, 0, 1)) {
 		int err = bt_le_scan_start(&scan_parameters,
 					   lcz_bt_scan_adv_handler);
-		LOG_DBG("%d", err);
 		if (err != 0) {
+			LOG_ERR("Unable to start scanning: %d", err);
 			atomic_clear(&bts.scanning);
 		} else {
+			LOG_DBG("%d", err);
 			bts.num_starts += 1;
 		}
 	}
@@ -166,7 +167,8 @@ static void lcz_bt_scan_adv_handler(const bt_addr_le_t *addr, int8_t rssi,
 	char bt_addr[BT_ADDR_LE_STR_LEN];
 	memset(bt_addr, 0, BT_ADDR_LE_STR_LEN);
 	bt_addr_le_to_str(addr, bt_addr, BT_ADDR_LE_STR_LEN);
-	LOG_DBG("Advert from %s RSSI: %d Type: %d", bt_addr, rssi, type);
+	LOG_DBG("Advert from %s RSSI: %d Type: %d", log_strdup(bt_addr), rssi,
+		type);
 	LOG_HEXDUMP_DBG(ad->data, ad->len, "Data:");
 #endif
 
